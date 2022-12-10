6322aafb10ef0a54f2b3c0151ec199ef807782fc
Trần Đại Đồng <120238379+trandaidong@users.noreply.github.com>
2022-12-11 01:24:27 +0700

    tutailen

---
+++ Bài-5b.cpp
@@ -0,0 +1,23 @@
+﻿// Viết hàm tính số Fibonacci thứ n không sử dụng đệ qui
+#include<iostream>
+using namespace std;
+int Fibonacci(int n)
+{
+	int F,F1,F2,F0;
+	F0 = 0;
+	F1 = F2 = 1;
+	for (int i = 3; i <= n; i++)
+	{
+		F = F1 + F2;
+		F1 = F2;
+		F2 = F;
+	}
+	return F;
+}
+int main()
+{
+	int n;
+	cout << "Nhap vao n = ";
+	cin >> n;
+	cout << "F" << "(" << n << ")" << " " << "=" << " " << Fibonacci(n);
+}
